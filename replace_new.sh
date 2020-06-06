1a8092dea806b3aa807b8216f60834ed6faf103b
Tomas Lukosius <lutomas@gmail.com>
2020-06-06 07:55:26 +0300

    Script for Indesign generated HTML fix.

---
+++ replace_new.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#perl -i.bak -pe '$count = 0; s/(\d)foo(\d)/(++$count % 2 == 0)?"$1FOO$2":$&/ge;' in.txt
+
+#perl -i.bak -pe '$count = 0; s/(story_content)/(++$count % 2 == 0)?"STORY":$&/ge;' in.txt
+
+#perl replace.pl StoriesText2.html
+#perl replace_styles.pl
+perl perl/replacer_new1.pl
+#perl replace2.pl
